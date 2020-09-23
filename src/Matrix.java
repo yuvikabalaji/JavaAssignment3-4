5f64317481c436221cb2404a9aa5c3663f10b176
yuvikabalaji <yuvikabalajee@gmail.com>
2020-09-23 10:50:53 +0530

    Java Assignment 3&4

--- src/Matrix.java
+++ src/Matrix.java
@@ -1,16 +1,15 @@
 public class Matrix {
     public static void main(String[] args) {
-        int[][] a={{1,2},{2,3}};
-        int[][] b={{3,4},{5,2}};
-        int[][] c=new int[2][2];
+        int a[][] = {{1, 2}, {2, 3}};
+        int b[][] = {{3, 4}, {5, 2}};
+        int c[][] = new int[2][2];
 
-        for (int i=0;i<2;i++) {
-            for (int j=0;i<2;i++) {
-                c[i][j] = a[i][j]+b[i][j];
-                System.out.print(c[i][j]+" ");
+        for (int i=0; i<2; i++) {
+            for (int j=0; j<2; j++) {
+                c[i][j] = a[i][j] + b[i][j];
+                System.out.print(c[i][j] + " ");
             }
             System.out.println();
         }
-
     }
 }
